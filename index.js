bdf0231223aa1e66cea1e36f59065a0744cfef73
Bin Xin <rhyzix@gmail.com>
2018-06-01 20:04:41 +0800

    watch deps

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-const requireFromString = require('require-from-string')
+// refs
+// https://stackoverflow.com/a/17585470/1793548
+// https://github.com/floatdrop/require-from-string
+const Module = require('module')
+const path = require('path')
 
-module.exports = function (content) {
-  return requireFromString(content, this.resourcePath)
+module.exports = function (code) {
+  const filename = this.resourcePath
+
+  const m = new Module(filename)
+  m.filename = filename
+  // m.paths = Module._nodeModulePaths(path.dirname(filename))
+  m._compile(code, filename)
+
+  // watch
+  const dep = ({ children }) => {
+    for (const m of children) {
+      this.addDependency(m.filename)
+      // clear cache
+      // https://stackoverflow.com/a/11477602/1793548
+      delete require.cache[m.filename]
+      if (m.children.length) dep(m)
+    }
+  }
+  dep(m)
+
+  const result = m.exports
+  if (typeof result !== 'string') throw new Error('exports must be a string')
+  return result
 }
